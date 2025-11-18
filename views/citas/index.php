ca5115ee689d305a5fee8d9172a9a31e340d1419
Tu Nombre <tucorreo@example.com>
2025-11-18 17:49:47 -0500

    Cambios en ambos paneles

--- views/citas/index.php
+++ views/citas/index.php
@@ -2,12 +2,14 @@
 
 <div class="d-flex">
     <div class="sidebar p-3 bg-dark text-white" style="width: 250px; min-height: 100vh;">
-        <h4 class="text-center mb-4 fw-bold">SG Admin</h4>
+        <h4 class="text-center mb-4 fw-bold">San Gabriel-Admin</h4>
         <a href="index.php?action=dashboard" class="text-secondary text-decoration-none d-block p-2"><i class="fas fa-chart-line me-2"></i> Dashboard</a>
         <a href="index.php?action=medicos" class="text-secondary text-decoration-none d-block p-2"><i class="fas fa-user-md me-2"></i> Médicos</a>
         <a href="index.php?action=pacientes" class="text-secondary text-decoration-none d-block p-2"><i class="fas fa-users me-2"></i> Pacientes</a>
         <a href="index.php?action=citas" class="text-white text-decoration-none d-block p-2 bg-secondary rounded mb-2"><i class="fas fa-calendar-check me-2"></i> Citas</a>
+        <a href="index.php?action=profile" class="text-secondary text-decoration-none d-block p-2"><i class="fas fa-user-cog me-2"></i> Reporte-PDF</a>
         <hr>
+        <a href="index.php?action=settings" class="text-secondary text-decoration-none d-block p-2"><i class="fas fa-cogs me-2"></i> Configuración</a>
         <a href="index.php?action=logout" class="text-danger text-decoration-none d-block p-2"><i class="fas fa-sign-out-alt me-2"></i> Cerrar Sesión</a>
     </div>
 
